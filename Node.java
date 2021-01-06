81e191e51aca19d61a37ddc3e769dca5ae91d0b3
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-06 22:02:59 +0000

    finished I think

--- Node.java
+++ Node.java
@@ -1,6 +1,33 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
- private String data;
- private Node next,prev;
+private String data;
+private Node next, prev;
+ public Node(String value){
+data = value;}
+
  //write get/set methods for all three instance variables.
+ 
+ public String getData(){
+   return data;
+ }
+ 
+public void setData (String data){
+  this.data = data;
+}
+
+public String getNext(){
+  return next;
+}
+
+public void setNext (Node next){
+ this.next = data;
+}
+
+public Node getPrev(){
+  return prev;
+}
+pubic void setPrev(Node prev){
+  this.prev = prev;
+}
+
+
 }
