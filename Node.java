4a627956aa63e8af271a5407eed43673bc90ecb8
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-07 17:18:59 +0000

    fixed identifier error?

--- Node.java
+++ Node.java
@@ -1,33 +1,29 @@
 public class Node{
-private String data;
-private Node next, prev;
- public Node(String value){
-data = value;}
+    private String data;
+    private Node next,prev;
 
- //write get/set methods for all three instance variables.
+    public Node(String value) {
+        data = value;}
  
- public String getData(){
-   return data;
- }
+       //write get/set methods for all three instance variables. 
  
-public void setData (String data){
-  this.data = data;
-}
+    public String getData() {
+        return data;}
 
-public String getNext(){
-  return next;
-}
+    public void setData(String data) {
+        this.data = data;}
 
-public void setNext (Node next){
- this.next = data;
-}
+    public Node getNext() {
+        return next;}
 
-public Node getPrev(){
-  return prev;
-}
-pubic void setPrev(Node prev){
-  this.prev = prev;
-}
+    public void setNext(Node next) {
+        this.next = next;}
 
+    public Node getPrev() {
+        return prev;}
 
+    public void setPrev(Node prev) {
+        this.prev = prev;}
+ 
+ 
 }
